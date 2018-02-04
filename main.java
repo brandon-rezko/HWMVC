38617de6423ca65118df70d30a8e1d5b354d6ca4
AFQSGASD12 <35571303+AFQSGASD12@users.noreply.github.com>
2018-02-04 16:50:45 +0200

    SRC Folder Files

---
+++ main.java
@@ -0,0 +1,15 @@
+import java.awt.EventQueue;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+HWMVC view = new HWMVC();
+Model model = new Model();
+ new Controller(view,model);
+
+view.show();
+
+	}
+
+}
