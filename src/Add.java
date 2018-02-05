aae02e42ef07cfc406e632c81ab39b359565d6c8
AFQSGASD12 <35571303+AFQSGASD12@users.noreply.github.com>
2018-02-06 01:06:33 +0200

    Add files via upload

--- src/Add.java
+++ src/Add.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import  org.junit.*;
 class Add {
 
 	@BeforeAll
--- src/Multi.java
+++ src/Multi.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import  org.junit.*;
 class Multi {
 
 	@BeforeAll
