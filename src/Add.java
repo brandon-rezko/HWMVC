cda8851c023cf877471ae62f31a6b9b94a4159c3
brandon-rezko <afqsgasd12@mail.com>
2024-10-05 12:00:00 +0800

    test(add): add test case for negative number addition

--- src/Add.java
+++ src/Add.java
@@ -1,18 +1,21 @@
 import static org.junit.Assert.assertEquals;
+import org.junit.*;
 
-
-
-import  org.junit.*;
 public class Add {
 
+    @Test
+    public void testPositiveAddition() {
+        Model model = new Model();
+        model.add(5, 5);
+        double result = model.returnresult();
+        assertEquals("Expected 5 + 5 to equal 10", 10, result, 0);
+    }
 
-
-	@Test
-	public void test() {
-		Model d = new Model();
-		 d.add(5,5);
-		 double as = d.returnresult();
-		 assertEquals(10,as,0);
-	}
-
+    @Test
+    public void testNegativeAddition() {
+        Model model = new Model();
+        model.add(-3, -7);
+        double result = model.returnresult();
+        assertEquals("Expected -3 + -7 to equal -10", -10, result, 0);
+    }
 }
