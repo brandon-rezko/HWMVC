fd08918777b198bae4a8c7631d6025a0c43798dc
brandon-rezko <afqsgasd12@mail.com>
2024-10-11 12:00:00 +0800

    test(add1): add test for positive and negative number addition

--- src/ADD1.java
+++ src/ADD1.java
@@ -4,13 +4,19 @@ import org.junit.Test;
 
 public class ADD1 {
 
-	@Test
-	
-	public void test() {
-		Model d = new Model();
-		 d.add(5,5);
-		 double as = d.returnresult();
-		 assertEquals(10,as,0);
-	}
+    @Test
+    public void testAddTwoPositiveNumbers() {
+        Model d = new Model();
+        d.add(5, 5);
+        double result = d.returnresult();
+        assertEquals("Expected 5 + 5 = 10", 10, result, 0);
+    }
 
+    @Test
+    public void testAddPositiveAndNegative() {
+        Model d = new Model();
+        d.add(10, -3);
+        double result = d.returnresult();
+        assertEquals("Expected 10 + (-3) = 7", 7, result, 0);
+    }
 }
